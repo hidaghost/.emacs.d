7ef2c550770a41730e082a3b9877652b153a5997
hida <hida_zen@qq.com>
2017-07-12 11:26:05 +0800

    update init.el, adding more packages settings include swiper & counsel

--- init.el
+++ init.el
@@ -13,12 +13,31 @@
 (require 'cl)
 ;; add whatever packages you want here
 (defvar hida/packages '(
+			;; auto complete
 			company
+
+			;; smex (enhance M-x)
+			;;smex
+
+			;; smart parens (smart insertion for parens)
+			hungry-delete
+			smartparens
+
+			;; swiper counsel (enhanced search & minibuffer complete tools)
+			swiper
+			counsel
+
+			;; js2-mode (for javascript coding)
+			js2-mode
+			
+			;; theme
 			monokai-theme
 			solarized-theme
 			)
   "Default packages"
   )
+(setq package-selected-packages hida/packages)
+
 (defun hida/packages-installed-p()
   (loop for pkg in hida/packages
 	when (not (package-installed-p pkg)) do (return nil)
@@ -35,11 +54,15 @@
     )
   )
 
+;; bind some usual keys for code search
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
 
 ;; open with full screen
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 
-;; show match parenthesis
+;; show match parenthesis(emacs lisp mode)
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
 ;; turn off splash screen
@@ -48,7 +71,7 @@
 ;; set F2 to quickly open init file
 (defun open-my-init-file()
   (interactive)
-  (find-file "~/.emacs"))
+  (find-file "~/.emacs.d/init.el"))
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 
 ;; turn off tool bar
@@ -65,20 +88,30 @@
 (global-hl-line-mode t)
 
 ;; open recent files
-()
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-item 10)
+;; (global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
 ;; cancel backup files save 
 (setq make-backup-files nil)
 
+;; load theme
+(load-theme 'solarized-light t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Org mode settings ;;;;
+;;;;;; mode settings ;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; highlight syntax
 (require 'org)
 (setq org-src-fontify-natively t)
 
+;; js2-mode
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Packages settings ;;;;
@@ -86,3 +119,22 @@
 
 ;; global company mode
 (global-company-mode t)
+
+;; smex settings
+;;(require 'smex)
+;;(smex-initialize)
+;;(global-set-key (kbd "M-x") 'smex)
+
+;; smart parens
+(require 'smartparens-config)
+(smartparens-global-mode t)
+
+;; swiper councel ivy settings
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
